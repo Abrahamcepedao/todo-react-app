baf7497875e5d2940179c3c4f8327fa7f6f36ac6
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-26 16:13:27 -0500

    Added add todo functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ todos, setTodos ] = useState(["item1", "item2"]);
+  const [ input, setInput ] = useState('');
+
+  const addTodo = (event) =>{
+    setTodos([...todos, input]);
+  };
+
   return (
     <div className="App">
       <h1>Hellow world</h1>
+      <input value={input}  onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
